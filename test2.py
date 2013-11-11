5f43f080f5955fc6ef9d6a925ddd873b2ae608d2
Bulwersator <bulwersator@gmail.com>
2013-11-11 18:45:06 +0100

    properly paintable play area

--- test2.py
+++ test2.py
@@ -7,18 +7,28 @@ screen = pygame.display.set_mode(size)
 
 black = 0, 0, 0
 red = 255, 0, 0
+cursor_data = black
 
-play_area_size = 100 #in tiles
-play_area_tile_size = 100 #in pixels
-play_area = [([red] * play_area_size)] * play_area_size
+class PlayArea:
+	def __init__(self, play_area_size, play_area_tile_size):
+		self.play_area_size = play_area_size #in tiles
+		self.play_area_tile_size = play_area_tile_size #in pixels
+		self.play_area = []
+		for i in range(0, play_area_size):
+			self.play_area.append([red] * play_area_size)
+	def press(self, x, y):
+		x/=self.play_area_tile_size
+		y/=self.play_area_tile_size
+		self.play_area[x][y] = cursor_data
+board = PlayArea(play_area_size=100, play_area_tile_size=100)
 
 class Button:
 	def __init__(self, position, size):
 		self.size = size
 		self.position = position
 		self.surface = pygame.Surface(self.size)
-		color = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
-		self.surface.fill(color)
+		self.color = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+		self.surface.fill(self.color)
 	def is_pressed(self, x, y):
 		if (self.position[0]) <= x <= (self.position[0] + self.size[0]):
 			if (self.position[1]) <= y <= (self.position[1] + self.size[1]):
@@ -47,6 +57,8 @@ while 1:
 			for thing in menu:
 				if thing.is_pressed(x, y):
 					touch = not touch
+					cursor_data = thing.color
+			board.press(x, y)
 	if touch:
 		x, y = pygame.mouse.get_rel()
 		cursor.position = (cursor.position[0]+x, cursor.position[1]+y)
@@ -58,14 +70,14 @@ while 1:
 		screen.blit(pygame.transform.scale(thing.surface, thing.size), pygame.Rect(thing.position, dummy))
 	x_min = 0
 	y_min = 0
-	x_max = (screen_width-button_width)/play_area_tile_size
-	y_max = screen_height/play_area_tile_size
+	x_max = (screen_width-button_width)/board.play_area_tile_size
+	y_max = screen_height/board.play_area_tile_size
 	for x in range(x_min, x_max):
 		for y in range(y_min, y_max):
-			size = (play_area_tile_size, play_area_tile_size)
-			position = (play_area_tile_size*(x-x_min), play_area_tile_size*(y-y_min))
+			size = (board.play_area_tile_size, board.play_area_tile_size)
+			position = (board.play_area_tile_size*(x-x_min), board.play_area_tile_size*(y-y_min))
 			surface = pygame.Surface(size)
-			color = play_area[x][y]
+			color = board.play_area[x][y]
 			surface.fill(color)
 			screen.blit(surface, pygame.Rect(position, dummy))
 	screen.blit(pygame.transform.scale(cursor.surface, cursor.size), pygame.Rect(cursor.position, dummy))

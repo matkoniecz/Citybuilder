e2da105d9e93d582d802ad0965f5efb3ab092dad
Bulwersator <bulwersator@gmail.com>
2013-11-15 19:36:53 +0100

    pack commands into functions

--- test2.py
+++ test2.py
@@ -4,6 +4,12 @@ pygame.init()
 
 size = screen_width, screen_height = 600, 600
 screen = pygame.display.set_mode(size)
+button_width = 100
+button_height = 100
+play_area_size = 100
+play_area_tile_size = 100
+cursor_size = 25
+touch = False
 
 black = 0, 0, 0
 red = 255, 0, 0
@@ -20,7 +26,20 @@ class PlayArea:
 		x/=self.play_area_tile_size
 		y/=self.play_area_tile_size
 		self.play_area[x][y] = cursor_data
-board = PlayArea(play_area_size=100, play_area_tile_size=100)
+	def add_to_screen(self, screen):
+		x_min = 0
+		y_min = 0
+		x_max = (screen_width-button_width)/self.play_area_tile_size
+		y_max = screen_height/self.play_area_tile_size
+		for x in range(x_min, x_max):
+			for y in range(y_min, y_max):
+				size = (self.play_area_tile_size, self.play_area_tile_size)
+				position = (self.play_area_tile_size*(x-x_min), self.play_area_tile_size*(y-y_min))
+				surface = pygame.Surface(size)
+				color = self.play_area[x][y]
+				surface.fill(color)
+				screen.blit(surface, pygame.Rect(position, dummy))
+		return screen
 
 class Button:
 	def __init__(self, position, size):
@@ -35,29 +54,35 @@ class Button:
 				return True
 		return False
 
-button_width = 100
-button_height = 100
-menu = []
-for i in range(0, screen_height/button_height):
-	menu.append(Button(position=(screen_width-button_width, button_height*i), size=(button_width, button_height)))
-
-
-cursor_size = 25
+class Menu:
+	def __init__(self, button_width, button_height):
+		self.menu = []
+		for i in range(0, screen_height/button_height):
+			self.menu.append(Button(position=(screen_width-button_width, button_height*i), size=(button_width, button_height)))
+	def press(self, x, y):
+		for thing in self.menu:
+			if thing.is_pressed(x, y):
+				global touch, cursor_data
+				touch = not touch
+				cursor_data = thing.color
+	def add_to_screen(self, screen):
+		for thing in self.menu:
+			screen.blit(pygame.transform.scale(thing.surface, thing.size), pygame.Rect(thing.position, dummy))
+		return screen
+		
+board = PlayArea(play_area_size, play_area_tile_size)
+menu = Menu(button_width, button_height)
 cursor = Button(position=(screen_width-button_width+(button_width-cursor_size)/2, (button_width-cursor_size)/2), size=(cursor_size, cursor_size))
 cursor.surface = pygame.image.load("rectangle.bmp")
 
 #print pygame.mouse.get_rel()
-touch = False
 while 1:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: sys.exit()
 		if event.type == pygame.MOUSEBUTTONDOWN:
 			# Set the x, y positions of the mouse click
 			x, y = event.pos
-			for thing in menu:
-				if thing.is_pressed(x, y):
-					touch = not touch
-					cursor_data = thing.color
+			menu.press(x, y)
 			board.press(x, y)
 	if touch:
 		x, y = pygame.mouse.get_rel()
@@ -66,19 +91,7 @@ while 1:
 		pygame.mouse.get_rel()
 	screen.fill(black)
 	dummy = (1, 1)
-	for thing in menu:
-		screen.blit(pygame.transform.scale(thing.surface, thing.size), pygame.Rect(thing.position, dummy))
-	x_min = 0
-	y_min = 0
-	x_max = (screen_width-button_width)/board.play_area_tile_size
-	y_max = screen_height/board.play_area_tile_size
-	for x in range(x_min, x_max):
-		for y in range(y_min, y_max):
-			size = (board.play_area_tile_size, board.play_area_tile_size)
-			position = (board.play_area_tile_size*(x-x_min), board.play_area_tile_size*(y-y_min))
-			surface = pygame.Surface(size)
-			color = board.play_area[x][y]
-			surface.fill(color)
-			screen.blit(surface, pygame.Rect(position, dummy))
+	screen = menu.add_to_screen(screen)
+	screen = board.add_to_screen(screen)
 	screen.blit(pygame.transform.scale(cursor.surface, cursor.size), pygame.Rect(cursor.position, dummy))
 	pygame.display.flip()
